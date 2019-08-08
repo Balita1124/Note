4681cd6313613fa26ed3b2f43b6008b222215639
Rico Flavien FAUCHARD ANDRIANAIVO <randrianaivo@dsogroup.com>
2019-08-08 16:55:11 +0400

    add

--- Angular/Reference/MEAN.js
+++ Angular/Reference/MEAN.js
@@ -257,11 +257,17 @@ exemple of complete code server.js
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';import Issue from './models/Issue';const app = express();
-const router = express.Router();app.use(cors());
-app.use(bodyParser.json());mongoose.connect('mongodb://[server]/issues');const connection = mongoose.connection;connection.once('open', () => {
+import mongoose from 'mongoose';
+import Issue from './models/Issue';
+const app = express();
+const router = express.Router();
+app.use(cors());
+app.use(bodyParser.json());
+mongoose.connect('mongodb://[server]/issues');
+const connection = mongoose.connection;connection.once('open', () => {
     console.log('MongoDB database connection established successfully!');
-});router.route('/issues/add').post((req, res) => {
+});
+router.route('/issues/add').post((req, res) => {
     let issue = new Issue(req.body);
     issue.save()
         .then(issue => {
@@ -270,21 +276,24 @@ app.use(bodyParser.json());mongoose.connect('mongodb://[server]/issues');const c
         .catch(err => {
             res.status(400).send('Failed to create new record');
         });
-});router.route('/issues').get((req, res) => {
+});
+router.route('/issues').get((req, res) => {
     Issue.find((err, issues) => {
         if (err)
             console.log(err);
         else
             res.json(issues);
     });
-});router.route('/issues/:id').get((req, res) => {
+});
+router.route('/issues/:id').get((req, res) => {
     Issue.findById(req.params.id, (err, issue) => {
         if (err)
             console.log(err);
         else
             res.json(issue);
     })
-});router.route('/issues/update/:id').post((req, res) => {
+});
+router.route('/issues/update/:id').post((req, res) => {
     Issue.findById(req.params.id, (err, issue) => {
         if (!issue)
             return next(new Error('Could not load Document'));
@@ -300,16 +309,16 @@ app.use(bodyParser.json());mongoose.connect('mongodb://[server]/issues');const c
             });
         }
     });
-});router.route('/issues/delete/:id').get((req, res) => {
+});
+router.route('/issues/delete/:id').get((req, res) => {
     Issue.findByIdAndRemove({_id: req.params.id}, (err, issue) => {
         if (err)
             res.json(err);
         else
             res.json('Removed successfully');
     });
-});app.use('/', router);app.listen(4000, () => console.log(`Express server running on port 4000`));
-
-
+});
+app.use('/', router);app.listen(4000, () => console.log(`Express server running on port 4000`));
 
 Setting up mongoDB
 =======================
